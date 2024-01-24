e47e6079b7333875282a903ee7ee3c582714d1eb
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2024-01-24 22:06:21 +0900

    Add files via upload
    구현 문제

---
+++ implementation.py
@@ -0,0 +1,23 @@
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+# 서,동,북,남
+move_types = ['L', 'R', 'U', 'D']
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+for plan in plans:
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+    # Check if the next position is within the grid boundaries
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+
+    # Update the current position if within the boundaries
+    x, y = nx, ny
+
+print(x, y)
+
