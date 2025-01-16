12772412778c0a85e7538b9bb707cb4e050e7e20
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-01-17 00:09:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,5 +1,6 @@
 def solution(emergency):
-    sorted_list = sorted(emergency,reverse=True)
-    sorted_dict={sorted_list.index(i)+1:i for i in emergency}
-    
-    return list(sorted_dict.keys())
+    sorted_emergency = sorted(emergency, reverse=True)
+    rank = {value: index + 1 for index, value in enumerate(sorted_emergency)}
+    answer = [rank[k] for k in emergency]
+
+    return answer
