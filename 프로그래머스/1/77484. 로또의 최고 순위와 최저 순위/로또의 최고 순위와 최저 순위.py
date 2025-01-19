4679283f12dac27f004004a4d71494d9192080d9
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-01-19 16:31:59 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,11 +1,12 @@
 def solution(lottos, win_nums):
-    zero_count=lottos.count(0)
-    remove_set = {0}
-    lottos = [i for i in lottos if i not in remove_set]
+    zero_count = lottos.count(0)  
+    win_nums = set(win_nums)  
+    lottos = set(lottos) - {0}  # 0을 제외한 로또 번호 집합
     
-    lottos=set(lottos)
-    if len(lottos)==0: return [1,6]
-    win_nums=set(win_nums)
-    count=len(lottos & win_nums) # 맞춘 개수
-
-    return [7-(count+zero_count) if count > 0 else 6, 7-count if count > 0 else 6] # 0이 맞음, 0이 틀림 
+    correct_count = len(lottos & win_nums)  # 맞춘 숫자 개수
+    
+    # 최고 등수와 최저 등수 계산
+    highest_rank = min(7 - (correct_count + zero_count), 6)
+    lowest_rank = min(7 - correct_count, 6)
+    
+    return [highest_rank, lowest_rank]
