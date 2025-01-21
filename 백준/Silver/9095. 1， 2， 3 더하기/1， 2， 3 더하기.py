ccb5e7d1c89d3c929ce394ab5f37dafd38d65958
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-01-22 00:13:20 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,31 +1,18 @@
 import sys
+T = int(sys.stdin.readline())
 
-count=int(sys.stdin.readline()) # 총 케이스
+for _ in range(T):
+    N = int(sys.stdin.readline())
+    dp = [0] * (N + 1)
 
-while count > 0 :
-    number=int(sys.stdin.readline())
-    answer=0
-    # 1, 2, 3 덧셈 계산
-    for n1 in range(1,4):
-        if n1 == number : answer+=1
-        for n2 in range(1,4):
-            if (n1+n2) == number : answer+=1
-            for n3 in range(1,4):
-                if(n1+n2+n3)==number : answer+=1
-                for n4 in range(1,4):
-                    if (n1+n2+n3+n4)==number : answer+=1
-                    for n5 in range(1,4):
-                        if(n1+n2+n3+n4+n5)==number : answer+=1
-                        for n6 in range(1,4):
-                            if(n1+n2+n3+n4+n5+n6)==number: answer+=1
-                            for n7 in range(1,4):
-                                if(n1+n2+n3+n4+n5+n6+n7)==number: answer+=1
-                                for n8 in range(1,4):
-                                    if(n1+n2+n3+n4+n5+n6+n7+n8)==number: answer+=1
-                                    for n9 in range(1,4):
-                                        if(n1+n2+n3+n4+n5+n6+n7+n8+n9)==number: answer+=1
-                                        for n0 in range(1,4):
-                                            if(n1+n2+n3+n4+n5+n6+n7+n8+n9+n0)==number: answer+=1
-    
-    print(answer) # 경우의 수 출력
-    count-=1
+    for i in range(1, N + 1):
+        if i == 1:
+            dp[i] = 1
+        elif i == 2:
+            dp[i] = 2
+        elif i == 3:
+            dp[i] = 4
+        else:
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[N])
