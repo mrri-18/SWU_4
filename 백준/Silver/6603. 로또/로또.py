56f21ddc9b13c02477b6071e7f48985b02cee901
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-01-24 17:33:22 +0900

    [Silver II] Title: 로또, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import combinations
+
+while True:
+    combi=list(map(int, sys.stdin.readline().split()))
+    n=combi[0]
+    if n == 0:
+        break
+    combi.remove(n)
+
+    for i in combinations(combi,6):
+        for x in list(i):
+            print(x,end=' ')
+        print('')
+    print(' ')
