a67eb90818f42ada63ad346240650ca204fb4706
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-01-29 00:34:23 +0900

    [Silver III] Title: N과 M (1), Time: 120 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,8 +1,20 @@
 import sys
-from itertools import permutations
+
+def back(index,arr):
+    if len(arr)==m: # 종료 조건
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(1,n+1):
+        if visited[i]:
+            continue
+        visited[i]=True
+        arr.append(i)
+        back(index+1,result)
+        arr.pop()
+        visited[i]=False
+
 
 n,m=map(int, sys.stdin.readline().split())
-for i in permutations(range(1,n+1),m):
-    for x in i:
-        print(x,end=' ')
-    print('')
+result=[]
+visited=[False]*(n+1)
+back(1,result)
