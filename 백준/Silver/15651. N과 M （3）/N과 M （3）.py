b5524fd5e1a1bb76f0929a6682bd8426c8ea1e91
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-01-29 02:12:44 +0900

    [Silver III] Title: N과 M (3), Time: 1116 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,18 @@
+import sys
+
+def back(index,arr):
+    if len(arr)==m: # 종료 조건
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(1,n+1):
+        visited[i]=True
+        arr.append(i) 
+        back(index,result)
+        arr.pop()
+        visited[i]=False
+
+
+n,m=map(int, sys.stdin.readline().split())
+result=[]
+visited=[False]*(n+1)
+back(1,result)
