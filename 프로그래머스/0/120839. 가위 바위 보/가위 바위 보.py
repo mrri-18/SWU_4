e42d55b8a2660124e12c9c1838a0cc136949d52c
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-01-29 22:53:16 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,8 +1,9 @@
 def solution(rsp):
     d = {'0': '5', '2': '0', '5': '2'}
-    result = []
-    for i in rsp:
-        result.append(d[i])  # 변환된 값을 리스트에 추가
-    return ''.join(result)   # 리스트를 문자열로 연결
+    rsp=list(rsp)
+    for i in range(len(rsp)):
+        rsp[i]=d[rsp[i]]
+        
+    return ''.join(rsp)
         
         
