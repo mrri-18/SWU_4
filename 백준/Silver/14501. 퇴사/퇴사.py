3d535dde7b096e88a77e11b86cee0b9bf073de89
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2025-02-02 18:01:41 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,26 @@
+import sys
+
+def back(index, add):
+    global max_income  
+    if index <= n:
+        max_income = max(max_income, sum(add))
+
+    if index>=n:
+        return
+    
+    for i in range(index,n):
+        if visited[i]: # 중복 비허용
+            continue
+        visited[i]=True
+        add.append(work[i][1])
+        back(i+work[i][0], add)
+        add.pop()
+        visited[i]=False
+n=int(sys.stdin.readline())
+work = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+max_income = 0  
+visited=[False]*(n+1)
+back(0, []) # 날짜, (소득)
+print(max_income)  
+
