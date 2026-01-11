64203935ea8778b94c5965506ae659a00c36b29c
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-11 11:45:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.16 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,4 @@
 from collections import Counter
 def solution(participant, completion):
-    counter=Counter(completion)
-    player=Counter(participant)
-    for i in counter.keys():
-        if counter[i]!=player[i]: # 동명이인 처리
-            return i
-        else:
-            del player[i]
+    player=Counter(participant)-Counter(completion)
     return list(player.keys())[0]
