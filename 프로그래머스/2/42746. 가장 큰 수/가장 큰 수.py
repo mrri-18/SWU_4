75abdde5857159185130af203c74a6851b7d53ee
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-13 11:36:58 +0900

    [level 2] Title: 가장 큰 수, Time: 1405.00 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,6 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
     
-    # 정렬 기준: x+y와 y+x를 비교
-    numbers.sort(key=lambda x: x*3, reverse=True)
+    numbers.sort(key=lambda x: x*3, reverse=True) # int 비교 x
     
     return str(int(''.join(numbers)))
-            
