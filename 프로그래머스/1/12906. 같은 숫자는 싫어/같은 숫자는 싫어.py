841fb4418ab1b0edd9abfeb9e1c07a9624ec0069
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-15 14:13:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.00 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,9 @@
 def solution(arr):
     answer=[arr[0]]
-    for i in arr:
-        if i==answer[-1]: 
+    for i in arr: 
+        if i==answer[-1]: # 연속적으로 같은 숫자인 경우 
             pass
-        else:
+        else: # 다른 숫자가 오는 경우
             answer.append(i)
     return answer
         
