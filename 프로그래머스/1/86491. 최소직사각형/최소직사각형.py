52a4949fd34e14c6d4167143d31ab64294603193
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-16 15:25:56 +0900

    [level 1] Title: 최소직사각형, Time: 4.12 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -4,7 +4,7 @@ def solution(sizes):
     
     #그리디
     for i,j in sizes:
-        width.append(max(i,j))
+        width.append(max(i,j)) # 데이터 단순화=> 가로가 긴 쪽으로 고정
         height.append(min(i,j))        
         
     return max(width)*max(height)
