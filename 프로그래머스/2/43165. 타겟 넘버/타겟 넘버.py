8be3abc7b7d1babf5905447fbbbe780fb541dfaf
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-17 12:35:46 +0900

    [level 2] Title: 타겟 넘버, Time: 510.47 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,21 @@
 def solution(numbers, target):
-    answer = 0
+    stack=[(0,0)] #curr_sum, idx
+    # visited=[False]*(len(numbers)+1)
+    answer=0
+    
+    while stack :
+        curr_sum, idx=stack.pop()
+        if idx==len(numbers):
+            if target==curr_sum:
+                answer+=1
+            else:
+                continue
+        # if not visited[idx]:
+        #     visited[idx]=True
+        else:
+            stack.append((curr_sum+numbers[idx],idx+1))
+            stack.append((curr_sum-numbers[idx],idx+1))
+            
 
-    def dfs(index, current_sum):
-        nonlocal answer
-        if index == len(numbers):  # 모든 숫자를 다 사용한 경우 
-            if current_sum == target:  
-                answer += 1
-            return        
-        # 현재 숫자를 더하거나 빼는 두 가지 경우=
-        dfs(index + 1, current_sum + numbers[index])  
-        dfs(index + 1, current_sum - numbers[index])  
 
-    dfs(0, 0)  # 0번 인덱스, 합은 0부터 시작
     return answer
