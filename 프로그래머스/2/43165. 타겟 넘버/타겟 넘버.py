c74dc403987dcf8ffac0311116cbbef53ae548fe
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-17 12:38:50 +0900

    [level 2] Title: 타겟 넘버, Time: 455.11 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,21 +1,16 @@
 def solution(numbers, target):
     stack=[(0,0)] #curr_sum, idx
-    # visited=[False]*(len(numbers)+1)
     answer=0
     
-    while stack :
+    while stack : 
         curr_sum, idx=stack.pop()
         if idx==len(numbers):
             if target==curr_sum:
                 answer+=1
             else:
                 continue
-        # if not visited[idx]:
-        #     visited[idx]=True
         else:
             stack.append((curr_sum+numbers[idx],idx+1))
             stack.append((curr_sum-numbers[idx],idx+1))
             
-
-
     return answer
