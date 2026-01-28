2a2b1fc52721cabe91d08a9630ac4814caf5734c
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-29 01:31:40 +0900

    [level 2] Title: H-Index, Time: 0.16 ms, Memory: 7.77 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,9 @@
+def solution(citations):
+    citations.sort()
+    n = len(citations)
+    for i in range(n):
+    # h_candidate: 현재 논문을 포함해 자신보다 많이 인용된 논문의 수
+        h_candidate = n - i 
+        if citations[i] >= h_candidate:
+            return h_candidate
+    return 0
