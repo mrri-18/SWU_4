ea0f7634c89692a0bf1558bbbf2f11c58bb6ce03
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-29 21:24:48 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 316 ms, Memory: 32472 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -2,18 +2,17 @@ import sys
 
 num=int(sys.stdin.readline())
 
-def recursion(s, l, r):
-    global count
-    count+=1
-    if l >= r: return 1
-    elif s[l] != s[r]: return 0
-    else: return recursion(s, l+1, r-1)
+def recursion(s, l, r,c):
+    c+=1
+    if l >= r: return (1, c)
+    elif s[l] != s[r]: return (0, c)
+    else: return recursion(s, l+1, r-1,c)
 
-def isPalindrome(s):
-    global count
-    count=0
-    return recursion(s, 0, len(s)-1)
+def isPalindrome(s,c):
+    return recursion(s, 0, len(s)-1,c)
 
 for i in range(num):
     strings=sys.stdin.readline().strip()
-    print(isPalindrome(strings), count)
+    count=0
+    pal, c=isPalindrome(strings,count)
+    print(pal, c)
