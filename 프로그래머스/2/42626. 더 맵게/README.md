a684e8080c8d287524219153716210eb9646a569
Lee Min Gyeong <80446737+mrri-18@users.noreply.github.com>
2026-01-29 20:35:05 +0900

    [level 2] Title: 더 맵게, Time: 2043.90 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 50.9 MB, 시간: 1925.18 ms
+메모리: 50.9 MB, 시간: 2043.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 20일 00:39:21
+2026년 01월 29일 20:35:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,14 +1,13 @@
 import heapq
 def solution(scoville, K):
-    answer = 0
+    count=0
     heapq.heapify(scoville)
-    while scoville[0]<K: # 최소 스코빌이 k 이하인 동안
-        if len(scoville)>=2 : # 음식 꺼내기 전 확인
-            tmp1=heapq.heappop(scoville)
-            tmp2=heapq.heappop(scoville)
-            heapq.heappush(scoville,tmp1+(tmp2*2))
-            answer+=1
-        else: # K이상 합칠 수 없는 경우
+    while scoville[0]<K:
+        if len(scoville)>=2:
+            tmp=heapq.heappop(scoville)
+            second=heapq.heappop(scoville)
+            heapq.heappush(scoville, tmp+second*2)
+            count+=1
+        else:
             return -1
-    
-    return answer
+    return count 
